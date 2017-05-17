0473826cc825b7d707f7944740ecfaef60f1b945
Byronizqui <byron15@hotmail.es>
2017-05-16 20:57:18 -0600

    Primer commit

---
+++ app/src/main/java/eti/domino/Conexion.java
@@ -0,0 +1,9 @@
+package eti.domino;
+
+/**
+ * Created by Byron on 16/5/2017.
+ */
+
+public class Conexion {
+
+}
---
+++ app/src/main/java/eti/domino/DominoActivity.java
@@ -0,0 +1,70 @@
+package eti.domino;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class DominoActivity extends Activity {
+    private GLSurfaceView openGLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        openGLView = new OpenGLView(this);
+        setContentView(openGLView);
+        new MyAsyncTask().execute(
+                "192.168.0.10",
+                "10578",
+                "Some");
+    }
+    
+    @Override
+    protected void onPause() {
+        super.onPause();
+        openGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        openGLView.onResume();
+    }
+
+    private class MyAsyncTask extends AsyncTask<String, Void, String> {
+        IOException ioException;
+        MyAsyncTask() {
+            super();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder sb = new StringBuilder();
+            try {
+                Socket socket = new Socket(
+                        params[0],
+                        Integer.parseInt(params[1]));
+                OutputStream out = socket.getOutputStream();
+                out.write(params[2].getBytes());
+                InputStream in = socket.getInputStream();
+                byte buf[] = new byte[1024];
+                int nbytes;
+                while ((nbytes = in.read(buf)) != -1) {
+                    sb.append(new String(buf, 0, nbytes));
+                }
+                socket.close();
+            } catch(IOException e) {
+                return "error";
+            }
+            return sb.toString();
+        }
+        @Override
+        protected void onPostExecute(String result) {
+
+        }
+    }
+}
